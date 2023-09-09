481ac93f2e75b56e5b8390b82fb20811487d98a5
Anthony <bob39537@gmail.com>
2023-09-09 16:17:28 -0400

    first commit

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -1,5 +1,5 @@
 import {useLocation} from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import axios from 'axios'
 
@@ -7,32 +7,97 @@ import '../css/play.css'
 
 const Play = ()=>{
 
-    const [gameReady, setReady] = useState(false) 
-    const [success, setSuccess] = useState(false) 
+    const [gameReady, setReady] = useState(true) 
+    const [success, setSuccess] = useState(true) 
+    const [index, setIndex] = useState(0) 
+    const [chosen, setChosen] = useState("none") 
+    const endQuiz = useRef(false)
 
-    const [answer, setAnswer] = useState('')
-    const [choices, setChoices] = useState([])
+    const [questionList, setQuestionList] = useState([
+        {
+            original: 'walking',
+            choices: ['hi', '2' , '3' , '4'],
+            answerIndex: 0,
+        },
+        {
+            original: 'running',
+            choices: ['hi', '2' , '3' , '4'],
+            answerIndex: 1,
+        }
+    ])
+
+    const [score, setScore] = useState(0)
+
+    const [done, setDone] = useState(false)
+
+    const finalScore = useRef(0)
 
     const location = useLocation()
 
     const numQuestions = location.state.number
     const language = location.state.language
 
+
+    const nextQuestion =()=>{
+        if(index < questionList.length - 1){
+            setIndex(index + 1)
+
+            setDone(false)
+            setChosen("none")
+
+        }
+        else{
+            endQuiz.current = true
+        }  
+
+    }
+
+    const showAnswer =()=> {
+        setDone(true)
+    }
+
+    const finishQuestion =()=> {
+
+        showAnswer()
+
+        setTimeout(nextQuestion, 1000)
+    
+    }
+
+
+    const wrongAnswer=(e, choice)=>{
+        if(done)
+            return
+
+        if(endQuiz.current)
+            return
+
+        if(choice !== questionList[index].answerIndex){
+          setChosen(e.target.id)
+          
+        }
+        else{
+            setScore(score + 1)
+            finalScore.current += 1
+
+        }
+        finishQuestion()
+    }
+
+
+
+
     const getGame =()=>{
 
-        const baseUrl = 'http://localhost:9999/question'
-        const request = axios.post(baseUrl,
-            {
-                'language': language,
-                'num_questions': numQuestions,
-            },
+        const baseUrl = 'http://localhost:9999/sentences'
+        const request = axios.get(baseUrl
+
         )
 
         request.then(response=>{
             if(response.status === 200){
                 setSuccess(true)
 
-                setAnswer(response.data)
                 console.log(response.data)
               }
 
@@ -73,17 +138,35 @@ const Play = ()=>{
     }
 
     return(
-    <div >
-        <div>
+    <div style={{display:'flex', flexDirection:'column', justifyContent:'flex-start', flex:'1 1 0'}}>
+        <div className="questionBox">
 
-            <p>The Game</p>
-            <p>{answer}</p>
+            <div style={{fontSize: '1.25rem'}} className=''>
+                {questionList[index].original} 
+            </div>
 
-            <ul className= 'list1' style = {{listStyleType: "none", overflow: 'auto',  minHeight:'20rem', flex:'1 1 0', marginBottom:'0'}}  >
-                {choices.map((choice, index) => 
-                  <li key={index}> {choice} </li>
-                )}
-            </ul>
+            <div style={{fontSize: '1.25rem'}} className=" ">
+                        
+                {language}
+                <br/><br/>
+                {score} / {questionList.length}
+            
+            </div>
+        </div>
+
+        <div id = "choice-container">
+                <button id="one" className = {(questionList[index].answerIndex === 0 && done === true)?  
+                  "buttonTop right" :
+                    chosen === "one"? "buttonTop wrong" : "buttonTop"} onClick={(e)=>wrongAnswer(e, 0)}>{questionList[index].choices[0]}</button>
+                <button id="two" className = {(questionList[index].answerIndex === 1 && done === true)?   
+                  "buttonTop right" :
+                    chosen === "two"? "buttonTop wrong" : "buttonTop"} onClick={(e)=>wrongAnswer(e, 1)}>{questionList[index].choices[1]}</button>
+                <button id ="three" className = {(questionList[index].answerIndex === 2 && done === true)? 
+                  "buttonTop right" :
+                    chosen === "three"? "buttonTop wrong" : "buttonTop"} onClick={(e)=>wrongAnswer(e, 2)}>{questionList[index].choices[2]}</button>
+                <button id="four" className = {(questionList[index].answerIndex === 3 && done === true)? 
+                  "buttonTop right" :
+                    chosen === "four"? "buttonTop wrong" : "buttonTop"} onClick={(e)=>wrongAnswer(e, 3)}>{questionList[index].choices[3]}</button>
         </div>
     </div>
     )
